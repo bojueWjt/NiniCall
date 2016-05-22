5da2dd1a88400f6f5da5c2399a9e0dfd261c7ff7
bojueWjt <jiataoTX@163.com>
2016-05-22 16:26:19 +0800

    fix

---
+++ IOSapp/components/ContactItem/index.js
@@ -0,0 +1,19 @@
+import React from 'react-native';
+
+const {
+  Component,
+  View,
+  Text,
+} = React;
+
+class ContactItem extends Component {
+  render() {
+    return (
+      <View>
+        <Text>ninico</Text>
+      </View>
+    );
+  }
+}
+
+export default ContactItem;
---
+++ IOSapp/containers/FriendsList.js
@@ -0,0 +1,22 @@
+import React from 'react-native';
+
+const {
+  Component,
+  View,
+  ScrollView,
+  Text,
+} = React;
+
+class FriendsList extends Component {
+  render() {
+    return (
+      <ScrollView>
+        <View>
+
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
+export default FriendsList;
